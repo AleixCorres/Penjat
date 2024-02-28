a19cf79c6c82c846ef17850dd22b868981cd945e
AleixCorres <145031923+AleixCorres@users.noreply.github.com>
2024-02-28 12:52:59 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -4,18 +4,25 @@ const maxMistakes = 7
 const numberCategories = 3
 const wordsByCategories = 8
 const numberCatCountrys = 0
-const numberCatDemonyms = 1
+const numberCatTravels = 1
 const numberCatCities = 2
 const separation = '_'
 let word
+let wordSeleced
 let categoryName
 let lettersUsed = []
 let final
+
+
+
+
+
 
 const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 
                   'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 
                   'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 //create variable for username (save this on variable)
+
 
 function saveUsername() {
 
@@ -31,34 +38,35 @@ function saveUsername() {
 }
 
 const categories = [
-  'countrys', 'demonyms', 'cities'
+  'countrys', 'travel', 'cities'
 ];
 
 //Create 3 arrays, each array have 15 words from 1 category
 
 const countries = [
-  ['CHINA', 'Largest country in East Asia','image/countries/china'],
-  ['GERMANY', 'European country known for its precision engineering', 'image/countries/germany'],
-  ['JAPAN', 'Island country in East Asia', 'image/countries/japan'],
-  ['BRAZIL', 'Largest country in South America', 'image/countries/brazil'],
-  ['CANADA', 'Second largest country in the world', 'image/countries/canada'],
-  ['AUSTRALIA', 'Country comprising the mainland of the Australian continent', 'image/countries/australia'],
-  ['ITALY', 'European country known for its art, architecture, and culture', 'image/countries/italy'],
-  ['SPAIN', 'European country known for its rich cultural heritage', 'image/countries/spain'],
-  ['KENYA', 'Country in East Africa known for its wildlife and scenic landscapes', 'image/countries/kenya']
+  ['CHINA', 'Largest country in East Asia','image/countries/china.png'],
+  ['GERMANY', 'European country known for its precision engineering', 'image/countries/germany.png'],
+  ['JAPAN', 'Island country in East Asia', 'image/countries/japan.png'],
+  ['BRAZIL', 'Largest country in South America', 'image/countries/brazil.png'],
+  ['CANADA', 'Second largest country in the world', 'image/countries/canada.png'],
+  ['AUSTRALIA', 'Country comprising the mainland of the Australian continent', 'image/countries/australia.png'],
+  ['ITALY', 'European country known for its art, architecture, and culture', 'image/countries/italy.png'],
+  ['SPAIN', 'European country known for its rich cultural heritage', 'image/countries/spain.png'],
+  ['KENYA', 'Country in East Africa known for its wildlife and scenic landscapes', 'image/countries/kenya.png']
 ]
 
 
-const demonyms = [
-  ['CHINESE', 'Of or related to China', 'image/demonyms/chinese'],
-  ['GERMAN', 'Of or related to Germany', 'image/demonyms/german'],
-  ['JAPANESE', 'Of or related to Japan', 'image/demonyms/japanese'],
-  ['CANADIAN', 'Of or related to Canada', 'image/demonyms/canadian'],
-  ['AUSTRALIAN', 'Of or related to Australia', 'image/demonyms/australian'],
-  ['ITALIAN', 'Of or related to Italy', 'image/demonyms/italian'],
-  ['SPANISH', 'Of or related to Spain', 'image/demonyms/spanish'],
-  ['KENYAN', 'Of or related to Kenya', 'image/demonyms/kenyan']
+const travels = [
+  ['Airplane', 'A mode of transportation that allows for fast travel over long distances through the air.', 'image/transportation/airplane'],
+  ['Boat', 'A watercraft designed to float, move, and carry passengers or cargo over water.', 'image/transportation/boat'],
+  ['Train', 'A form of transportation that runs on tracks and is powered by electricity, diesel, or steam.', 'image/transportation/train'],
+  ['Navigation', 'The process of planning and controlling the movement of a vehicle or vessel from one place to another.', 'image/transportation/navigation'],
+  ['Reservations', 'The act of booking or reserving seats, accommodations, or services in advance.', 'image/transportation/reservations'],
+  ['Luggage', 'The bags, suitcases, and belongings that a traveler carries with them during a trip.', 'image/transportation/luggage'],
+  ['Layovers', 'A period of time spent at an intermediate point during a journey when changing vehicles or waiting for a connecting flight.', 'image/transportation/layovers'],
+  ['Transfers', 'The process of moving from one mode of transportation to another, typically during a journey involving multiple legs.', 'image/transportation/transfers']
 ];
+
 
 const cities = [
   ['BEIJING', 'Capital of China', 'image/cities/beijing'],
@@ -71,32 +79,42 @@ const cities = [
   ['MOMBASA', 'City in Kenya', 'image/cities/mombasa']
 ]
 
+updateFirstMistakes();
 
 
-//Create random for categories and later for words
 
-let randomCategory = Math.floor(Math.random() * numberCategories);
-console.log(randomCategory)
+  let randomCategory = Math.floor(Math.random() * numberCategories);
+  console.log(randomCategory)
+  
+  let randomword = Math.floor(Math.random() * wordsByCategories);
+  console.log(randomword)
+  
+  //select categorie and word
+  const showCategory = document.querySelector("#categoryName")
+  
+  if (randomCategory === numberCatCountrys) {
+    wordSeleced = countries[randomword]
+    word = wordSeleced[0];
+  
+    categoryName = "Country"
+  
+  } else if (randomCategory === numberCatTravels) {
+    wordSeleced = travels[randomword]
+    word = wordSeleced[0];
+  
+    categoryName = "Travel"
+  
+  } else if (randomCategory === numberCatCities) {
+    wordSeleced = cities[randomword]
+    word = wordSeleced[0];
+  
+    categoryName = "City"
+  
+  }
+  
 
-let randomword = Math.floor(Math.random() * wordsByCategories);
-console.log(randomword)
 
-//select categorie and word
-const showCategory = document.querySelector("#categoryName")
 
-if (randomCategory === numberCatCountrys) {
-  word = countrys[randomword]
-  categoryName = "Country"
-
-} else if (randomCategory === numberCatDemonyms) {
-  word = demonyms[randomword]
-  categoryName = "Demonym"
-
-} else if (randomCategory === numberCatCities) {
-  word = cities[randomword]
-  categoryName = "City"
-
-}
 
 //Create wordmkask and checkerWord
 let mask = createMask()
@@ -104,6 +122,12 @@ let mask = createMask()
 //Create alphabet buttons
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  if(localStorage.getItem('userData')) {
+    let savedUserData = JSON.parse(localStorage.getItem('userData'));
+    
+    mistakes = savedUserData.mistakes;
+}
 
   refreshWord();
   showCategoryHTML();
@@ -135,7 +159,26 @@ function clickOnLetters() {
       
         const resultSection = document.querySelector("#resultSection");
         resultSection.style.display = "block"; 
+
+        let paragraph1 = document.createElement("p");
+        let paragraph2 = document.createElement("p");
+        let paragraph3 = document.createElement("p");
+        let img = document.createElement("img");
+
+
+        // Añade texto y clases a los elementos
+        paragraph1.textContent = "La paraula era" + wordSeleced[0];
+        paragraph2.textContent = "Has encertat la paraula";
+        paragraph3.textContent = wordSeleced[1];
+        img.src = wordSeleced[2]; 
       
+        // Adjunta los elementos al div resultSection
+        resultSection.appendChild(paragraph1);
+        resultSection.appendChild(paragraph2);
+        resultSection.appendChild(paragraph3);
+        resultSection.appendChild(img);
+
+        localStorage.clear();
       }
     });
   });
@@ -164,6 +207,17 @@ function updateLettersUsedonHTML(clickedLetter) {
   letterSpan.textContent = clickedLetter;
   lettersUsed.appendChild(letterSpan);
 
+}
+function updateFirstMistakes() {
+  let mistakesImage = document.getElementById("mistakesImage");
+  
+  if (mistakesImage.children.length === 0) {
+    let imageToAppend = new Image();
+    imageToAppend.src = 'image/'+ mistakes +'.png';
+    mistakesImage.appendChild(imageToAppend);
+  } else {
+    mistakesImage.children[0].src = 'image/'+ mistakes +'.png';
+  }
 }
 
 
@@ -227,9 +281,10 @@ function createLettersButtons() {
 
 function updateMistakes() {
   let mistakesImage = document.getElementById("mistakesImage");
+  let imageToRemove = mistakesImage.querySelector("img");
   let imageToAppend = new Image();
   imageToAppend.src = 'image/'+ mistakes +'.png';
-  mistakesImage.removeChild(mistakesImage.firstChild)
+  mistakesImage.removeChild(imageToRemove)
   mistakesImage.appendChild(imageToAppend)
 
 }
@@ -249,6 +304,9 @@ function checkletter(clickedLetter) {
     mistakes++;
   }
   lettersUsed.push(clickedLetter)
+
+
+
 }
 
 function refreshWord() {
@@ -263,3 +321,14 @@ function refreshWord() {
   });
 }
 
+
+window.addEventListener('beforeunload', function (e) {
+
+  var userData = {
+    mistakes: mistakes
+
+  };
+
+
+  localStorage.setItem('userData', JSON.stringify(userData));
+});
